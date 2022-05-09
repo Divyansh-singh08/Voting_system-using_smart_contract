8116fc61271f234c4983d9a33f4a36e271c7d2bf
Divyansh Singh <divyanshsinghhacked11@gmail.com>
2022-05-09 15:45:44 +0530

    voting system

---
+++ BlockVote/truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    "live": {
+      network_id: 5777,
+      host: "127.0.0.1",
+      port: 7546   // Different than the default below
+    }
+  },
+  rpc: {
+    host: "127.0.0.1",
+    port: 7545
+  }
+};
